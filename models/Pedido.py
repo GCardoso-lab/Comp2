07129e9d013cc1cd4e8a6ab384e7c00f5ce94b3b
guibarao <baraoguilherme13@gmail.com>
2022-07-24 22:06:55 -0300

    Criei uma classe produtos e mesclei as classes pedido e pagamento

--- models/Pedido.py
+++ models/Pedido.py
@@ -1,36 +1,80 @@
-class Pedido:
-    def __init__(self, estoque, valor, quantidade, produto):
-        self.Estoque = estoque
-        self.Valor = valor
-        self.Quantidade = quantidade
-        self.Produto = produto
+from Produtos import Produto
+from prettytable import PrettyTable
+from numpy.random import randint
 
-    def cardapio(cod):
-    menu = [(100,'Pipoca Pequena',12.00), (200,'Pipoca Media', 15.00), (300, 'Pipoca Grande', 20.00),
-    (400,'Refrigerante Pequeno', 7.00), (500, 'Refrigerante Medio', 9.00), (600, 'Refrigerante Grande', 11.00), (700, 'Agua', 4.00)]
-     
-    for c, sand, pr in menu:
-        if c == cod:
-           return sand, pr 
-    return '',0.0
-def imprimenota(ped):
-    tot = 0
-    totg = 0
-    print('Pedido            Quantidade        V.Unit        Total')
-    
-    for (sand,qtd,pr) in pedido:
-        tot = qtd * pr 
-        print(sand, (24 - len(sand))*' ', qtd, '           ', pr, '        ', tot)
-        totg += tot
-    print('                                    total da Nota: ', totg)
-          
-pedido = []
-cod = int(input('Digite o código do cardapio ou -1 para fechar o pedido:'))
-while cod != -1:
-     sand, pr = cardapio(cod)
-     if sand != '':
-        qtd = int(input('Digite a quantidade:'))
-        pedido.append((sand,qtd,pr)) 
-     cod = int(input('Digite o código do cardapio ou -1 para fechar o pedido:'))
-imprimenota(pedido)
-        
+
+class Pedido:
+    def __init__(self):
+        self.produtos = []
+        self.total = 0
+
+    def realizar_pedido(self):
+        cardapio = Produto("null", 0)
+        while True:
+            cardapio.recuperar_produtos()
+            i = 0
+            for produto in cardapio.produtos:
+                i = i + 1
+                print(i, " ", produto.get_nome())
+            opt = int(input("Digite um número para adicionar um produto ao carrinho, ou 0 para fechar"
+                            " o pedido: "))
+            if 1 <= opt <= i:
+                produto_escolhido = cardapio.produtos.pop(opt - 1)
+                self.produtos.append(produto_escolhido)
+                print("Produto adicionado!")
+            elif opt == 0:
+                total = 0
+                for produto in self.produtos:
+                    total = total + int(produto.get_preco())
+                self.total = total
+                print("Pedido Fechado!")
+                break
+            else:
+                print("Resposta Inválida.")
+
+    def imprimenota(self):
+        for produto in self.produtos:
+            print(f"{produto.get_nome()}:  {produto.get_preco()} R$")
+        print(f'\n\nTotal da Nota: {self.total} R$')
+
+    def pagar(self):
+        print("\n\nEscolha um método de pagamento:\n\n[ 1 ] Boleto Bancário\n[ 2 ] Cartão de Crédito ou Débito\n\n")
+        pagamento = int(input("Digite o número desejado: "))
+        if pagamento == 1:
+            print(f"Total a pagar: {self.total}")
+            self.gerar_boleto()
+        elif pagamento == 2:
+            parcelar = input("Deseja parcelar em até 3x sem juros? (S/N)")
+            if parcelar == "N":
+                print(f"Total a pagar: {self.total}")
+                numero = input("Informe o número do seu cartão de crédito (apenas números, sem espaço): ")
+                validade = input("Informe a data de expiração do cartão: (formato DD/MM/AA)")
+                cvv = input("Informe o código de verificação de 3 dígitos: ")
+                self.pagar_cartao(numero, validade, cvv)
+            elif parcelar == "S":
+                while True:
+                    numero_parcelas = int(input("Digite o número de vezes que deseja parcelar:"))
+                    if 1 <= numero_parcelas <= 3:
+                        print(f"Total a pagar: {self.total}/{numero_parcelas} durante {numero_parcelas} meses.\n\n")
+                        numero = input("Informe o número do seu cartão de crédito (apenas números, sem espaço): ")
+                        validade = input("Informe a data de expiração do cartão (formato DD/MM/AA): ")
+                        cvv = input("Informe o código de verificação de 3 dígitos: ")
+                        self.pagar_cartao(numero, validade, cvv)
+                        break
+                    else:
+                        print("Número inválido! Escolha um valor de 1 à 3.")
+
+    def gerar_boleto(self):
+        boleto = PrettyTable()
+        boleto.field_names = ["Produto", "Preço"]
+        for produto in self.produtos:
+            print(produto.get_nome())
+            print(produto.get_preco())
+            boleto.add_row([produto.get_nome(), produto.get_preco()])
+        boleto.add_row(["TOTAL:", f"{self.total} RS$"])
+        boleto.add_row(["CÓDIGO DO BOLETO:", randint(100000000, 999999999, 1)])
+        print(boleto)
+
+    def pagar_cartao(self, numero, validade, cvv):
+        print(f"{numero}, {validade}, {cvv}")
+        print("funcao em desenvolvimento.")
---
+++ models/Produtos.py
@@ -0,0 +1,85 @@
+class Produto:
+    def __init__(self,nome,preco):
+        self.nome = nome
+        self.preco = preco
+        self.arquivo = None
+        self.nome_arquivo = 'produtos.txt'
+        self.produtos=[]
+    def get_nome(self):
+        return self.nome
+
+    def get_preco(self):
+        return self.preco
+
+    def abre_arq_produtos(self, tipo):
+        try:
+            self.arquivo = open(self.nome_arquivo, tipo)
+            return True
+        except IOError:
+            print("Problemas para acessar a base de filmes!")
+            return False
+
+    def fecha_arq_produtos(self):
+        try:
+            self.arquivo.close()
+            return True
+        except IOError:
+            print("Problemas para acessar a base de filmes!")
+            return False
+
+    def le_arq_produtos(self, linha):
+        try:
+            self.arquivo.read()
+            return linha
+        except IOError:
+            print("Problemas para acessar a base de produtos!")
+            return False
+
+    def escreve_arq_produtos(self, linha):
+        try:
+            self.arquivo.write(linha)
+            return True
+        except IOError:
+            print("Problemas para acessar a base de produtos!")
+            return False
+
+    def recuperar_produtos(self):
+        self.produtos.clear()
+        if self.abre_arq_produtos('r'):
+            for line in self.arquivo:
+                pos_old = 0
+                funci = []
+                while True:
+                    try:
+                        pos = line.index(";", pos_old)
+                        funci.append(line[pos_old:pos])
+                        pos_old = pos + 1
+                    except ValueError:
+                        break
+
+                try:
+                    nome = funci.pop(0)
+                    preco = funci.pop(0)
+                    produto = Produto(nome, preco)
+                    self.produtos.append(produto)
+                except IndexError:
+                    self.fecha_arq_produtos()
+                    break
+            self.fecha_arq_produtos()
+
+    def registrar_produto(self):
+        print(f"\n\n          Produto: {self.nome}, {self.preco} R$")
+        while True:
+            confirma = input("\n\nConfirma inclusão de Produto? (S/N)\n\n")
+            if confirma == "S":
+                if self.abre_arq_produtos('a'):
+                    linha = f"{self.nome};{self.preco}; \n"
+                    self.escreve_arq_produtos(linha)
+                    self.fecha_arq_produtos()
+                    print("\nProduto incluído com sucesso.")
+                    break
+            elif confirma == "N":
+                print("Inclusão Cancelada.")
+                break
+            else:
+                print("Opção Inválida! Digite S ou N.")
